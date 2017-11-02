c97d7e427af1517c8e33a9ed054b2c00e74b2e2d
Aidan Mitchell <github@aidanmitchell.uk>
2017-11-02 10:49:08 +0000

    added global envs

---
+++ gambit/context_processors.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+
+def global_settings(request):
+    return {
+        "APPLICATION_VERSION": settings.APPLICATION_VERSION,
+        "APPLICATION_NAME": settings.APPLICATION_NAME
+    }
