25608dc28408ed432be03ceddc0fcd7e6ec45353
Aidan Mitchell <github@aidanmitchell.uk>
2017-11-03 17:16:53 +0000

    added signup and account activation flow

--- gambit/views.py
+++ gambit/views.py
@@ -42,3 +42,45 @@ class ServerErrorView(ErrorView):
     def __init__(self):
         self.error_value = 500
         self.error_type = 'Server Error'
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate your 44CON CFP account'
+            message = render_to_string('gambit/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, 'gambit/signup.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('index')
+    else:
+        return render(request, 'gambit/account_activation_invalid.html')
+
+
+def account_activation_sent(request):
+    return render(request, 'gambit/account_activation_sent.html')
