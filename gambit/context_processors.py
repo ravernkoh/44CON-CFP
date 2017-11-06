ed0b3e4d5ea96f9d504c67f2b888bcb032c62720
Aidan Mitchell <github@aidanmitchell.uk>
2017-11-06 14:07:00 +0000

    added more site-wide environment variables

--- gambit/context_processors.py
+++ gambit/context_processors.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 def global_settings(request):
     return {
-        "APPLICATION_VERSION": settings.APPLICATION_VERSION,
-        "APPLICATION_NAME": settings.APPLICATION_NAME
+        'APPLICATION_VERSION': settings.APPLICATION_VERSION,
+        'APPLICATION_NAME': settings.APPLICATION_NAME,
+        'CONFERENCE_YEAR': settings.CONFERENCE_YEAR
     }
--- gambit/settings/development.py
+++ gambit/settings/development.py
@@ -1,5 +1,7 @@
-from base import *
+from .base import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+CONFERENCE_YEAR = '2018'
 
 AUTH_PASSWORD_VALIDATORS[0].update({'OPTIONS': { 'min_length': 12 }})
