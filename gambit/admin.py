3e411ab8903f567c1b9af789bec9238845e01487
Aidan Mitchell <github@aidanmitchell.uk>
2017-11-07 16:29:54 +0000

    added refactored views for models in administration panel

--- gambit/admin.py
+++ gambit/admin.py
@@ -1,5 +1,49 @@
 from django.contrib import admin
+from django.template import defaultfilters
+from django.core.urlresolvers import reverse
 
 from .models import *
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'username',)
+
+    def username(self, obj):
+        return '<a href=\'{0!s}\'>{1!s}</a>'.format(reverse('admin:auth_user_change', args=(obj.user.id,)), obj.user.username)
+    username.allow_tags = True
+    username.short_description = 'User'
+
+
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'username', 'timestamp',)
+
+    def username(self, obj):
+        return '<a href=\'{0!s}\'>{1!s}</a>'.format(reverse('admin:auth_user_change', args=(obj.user.id,)), obj.user.username)
+    username.allow_tags = True
+    username.short_description = 'User'
+
+    def timestamp(self, obj):
+        return defaultfilters.date(obj.submitted_on, 'Y-m-d H:i')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title',)
+
+    def title(self, obj):
+        return obj.submission.title
+    title.short_description = 'Submission Title'
+
+
+class FrontPageAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'id',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=FrontPage):
+        return False
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(FrontPage, FrontPageAdmin)
