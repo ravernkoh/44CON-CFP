8674bf6ec93e75021bd72db7c502430f2ef2971b
Aidan Mitchell <github@aidanmitchell.uk>
2017-11-10 09:30:05 +0000

    added minor formatting changes

--- gambit/settings/development.py
+++ gambit/settings/development.py
@@ -1,5 +1,6 @@
 from .base import *
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CONFERENCE_YEAR = '2018'
@@ -8,6 +9,7 @@ CONFERENCE_YEAR = '2018'
 AUTH_PASSWORD_VALIDATORS[0].update({'OPTIONS': { 'min_length': 12 }})
 
 CONTENT_TYPES = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+
 # 2MiB = 2621440
 # 5MiB = 5242880
 # 10MiB = 10485760
