97ff1dbf52eeb6052980f8cc64fce1bc29aa4482
Aidan Mitchell <github@aidanmitchell.uk>
2017-12-06 08:18:43 +0000

    swapped database from sqlite to postgresql

--- gambit/settings/base.py
+++ gambit/settings/base.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'gambit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': path.join(BASE_DIR, 'gambit.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
