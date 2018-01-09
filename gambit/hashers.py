09c78cede5312d3b1556ed0021c5037df7592dd4
Aidan Mitchell <github@aidanmitchell.uk>
2018-01-09 17:31:28 +0000

    downgraded hashing iterations; not viable for production

--- gambit/hashers.py
+++ gambit/hashers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.hashers import BCryptSHA256PasswordHasher
 class ParanoidBCryptSHA256PasswordHasher(BCryptSHA256PasswordHasher):
     """A subclass of BCryptSHA256PasswordHasher to increase iterations"""
 
-    rounds = BCryptSHA256PasswordHasher.rounds * 1.3
+    rounds = BCryptSHA256PasswordHasher.rounds * 1.1
