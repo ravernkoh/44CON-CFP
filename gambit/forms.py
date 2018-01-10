a6f94d92fb17b8543f3fd69763f5985d6beace94
Aidan Mitchell <github@aidanmitchell.uk>
2018-01-10 11:13:48 +0000

    minor formatting for pep-8 compliance

--- gambit/forms.py
+++ gambit/forms.py
@@ -53,9 +53,12 @@ class SubmitForm(forms.ModelForm):
     def clean_file(self):
         # Reject file if it's not a valid content type or if it's too large
         file = self.cleaned_data["file"]
-        """If type(file) == FieldFile, then the file has already been uploaded during submission or a previous edit and does not need to be cleaned. It will throw an AttributeError on the content_type assignment because the model does not have such a field. This may happen when the user tries to change the uploaded file but a ValidationError occurs. If the user then chooses not to select a different file but clicks submit to update the submission, clean_file() will attempt to process the file which is actually a FieldFile causing the error described before.
-        """
-        if file and type(file) != FieldFile:
+        # If type(file) == FieldFile, then the file has already been uploaded during submission or a previous edit and does not need to be cleaned.
+        # It will throw an AttributeError on the content_type assignment because the model does not have such a field.
+        # This may happen when the user tries to change the uploaded file but a ValidationError occurs.
+        # If the user then chooses not to select a different file but clicks submit to update the submission, clean_file() will attempt to process the
+        # file which is actually a FieldFile causing the error described before.
+        if file and isinstance(file, FieldFile):
             content_type = file.content_type
             if content_type in settings.CONTENT_TYPES:
                 if file.size > settings.MAX_UPLOAD_SIZE:
@@ -83,8 +86,11 @@ class SubmitForm(forms.ModelForm):
             'abstract': 'Optional but preferred - Can be a brief outline (supported by an uploaded document) or detailed overview of your submission',
             'authors': 'Optional - Names of additional authors',
             'contact_email': 'Required - Email address that 44CON Speaker Ops should use for contact',
-            'conflicts': 'Optional - If you have any conflicts of interest with the panel - i.e. you are aware of having worked with, co-authored, or co-presented with panel members, please list their names here. It helps us ensure a fair process is followed.',
-            'file': 'If you have any specific requirements, constraints, supporting content, or just pictures of your cat then please upload them using this form.<br />If you have multiple files to upload, the form will accept a zip file.<br />File size limit: 50MiB. Permitted data types: pdf, doc/x, ppt/s, zip',
+            'conflicts': 'Optional - If you have any conflicts of interest with the panel - i.e. you are aware of having worked with, co-authored, or \
+            co-presented with panel members, please list their names here. It helps us ensure a fair process is followed.',
+            'file': 'If you have any specific requirements, constraints, supporting content, or just pictures of your cat then please upload them using \
+            this form.<br />If you have multiple files to upload, the form will accept a zip file.<br />File size limit: 50MiB. Permitted data \
+            types: pdf, doc/x, ppt/s, zip',
         }
         widgets = {
             'abstract': forms.Textarea(attrs={'rows': 8, 'cols': 60}),
