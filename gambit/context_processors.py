7679bed70e52d45872a88c4531738291c79c61b5
Aidan Mitchell <github@aidanmitchell.uk>
2018-01-15 14:30:17 +0000

    redesigned home layout
    * added notification bar for deadline
    * converted login and logout icons to buttons

--- gambit/context_processors.py
+++ gambit/context_processors.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from .models import FrontPage
+
 
 # Globally-accessible custom variables
 # These all require matching declarations in settings.py
@@ -8,4 +10,5 @@ def global_settings(request):
         'APPLICATION_VERSION': settings.APPLICATION_VERSION,
         'APPLICATION_NAME': settings.APPLICATION_NAME,
         'CONFERENCE_YEAR': settings.CONFERENCE_YEAR,
+        'SUBMISSION_DEADLINE': FrontPage.objects.filter(id=1)[0].submission_deadline,
     }
