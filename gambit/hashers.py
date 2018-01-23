48fa8b37ffc3bc03572c76467cfb30df9800b7fd
Aidan Mitchell <github@aidanmitchell.uk>
2018-01-23 11:02:52 +0000

    reduced work factor for bcrypt hasher

--- gambit/hashers.py
+++ gambit/hashers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.hashers import BCryptSHA256PasswordHasher
 class ParanoidBCryptSHA256PasswordHasher(BCryptSHA256PasswordHasher):
     """A subclass of BCryptSHA256PasswordHasher to increase iterations"""
 
-    rounds = BCryptSHA256PasswordHasher.rounds * 1.1
+    rounds = BCryptSHA256PasswordHasher.rounds + 0.5
