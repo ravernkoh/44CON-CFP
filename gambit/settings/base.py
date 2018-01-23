3b07d152aa273570682524d00291482065f2617e
Aidan Mitchell <github@aidanmitchell.uk>
2018-01-23 15:15:38 +0000

    moved javascript and css assets to be managed by bower

--- gambit/settings/base.py
+++ gambit/settings/base.py
@@ -148,6 +148,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../bower_components'),
+    os.path.join(BASE_DIR, 'assets')
+]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = "home"
