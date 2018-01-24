cc759c46e403b1feaf85516458820411ec7b9ca9
Aidan Mitchell <github@aidanmitchell.uk>
2018-01-24 15:19:20 +0000

    minor corrections

--- gambit/settings/production.py
+++ gambit/settings/production.py
@@ -1,11 +1,1 @@
 from .base import *
-
-
-try:
-    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-    DATABASES['default']['USER'] = os.environ["DJANGO_DATABASE_USER"]
-    DATABASES['default']['PASSWORD'] = os.environ["DJANGO_DATABASE_PASSWORD"]
-    DATABASES['default']['PORT'] = os.environ["DJANGO_DATABASE_PORT"]
-except KeyError as e:
-    print(f"\nEnvironment variable not set! {e!r}\n")
-    raise SystemExit(1)
