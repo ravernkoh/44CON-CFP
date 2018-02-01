9a4eaa6c6cc159e06eb8c02167459d84d9fcb467
Aidan Mitchell <github@aidanmitchell.uk>
2018-02-01 17:07:44 +0000

    minor usability changes

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -2,6 +2,7 @@
 # If you run this outside of the CFP project root, you can deal with the consequences
 
 set -eu
+set -o pipefail
 
 readonly PROJECT_ROOT=$(pwd)
 readonly SOURCE_MAP_DEST="$PROJECT_ROOT/bower_components/flat-ui/dist/css/flat-ui-44con-css.min.map"
@@ -10,13 +11,13 @@ readonly CSS_DEST="$PROJECT_ROOT/bower_components/flat-ui/dist/css/flat-ui-44con
 
 main() {
   echo '[+] Installing bower assets from bower.json'
-  bower install --save --production || { echo 'bower asset collection failed'; exit 1; }
+  bower install --save --production || { echo '[!] Failed to install bower assets'; exit 1; }
   echo '[>>] Copying less variables into flat-ui'
-  cp variables.less bower_components/flat-ui/less || { echo 'could not copy variables file to less directory'; exit 1; }
+  cp variables.less bower_components/flat-ui/less || { echo '[!] Could not less variables file'; exit 1; }
   echo '[+] Generating minifed CSS and source map for flat-ui'
-  lessc --clean-css --source-map="$SOURCE_MAP_DEST" "$LESS_SRC" "$CSS_DEST" || { echo 'could not generate css or map'; exit 1; }
+  lessc --clean-css --source-map="$SOURCE_MAP_DEST" "$LESS_SRC" "$CSS_DEST" || { echo '[!] lessc could not generate CSS or source map]'; exit 1; }
   echo '[>>] Collecting bower assets to Django static directory'
-  python manage.py collectstatic --clear || { echo 'could not collect static files'; exit 1; }
+  python manage.py collectstatic --clear || { echo '[!] Django could not collect static assets'; exit 1; }
   echo '[=)] Finished preparing style assets for project'
 }
 
