f5e5d3eef4d407b5b7e9897a7b97bf6e9220d0e6
Aidan Mitchell <github@aidamitchell.uk>
2018-02-02 15:24:33 +0000

    added middleware to pass 404s to sentry.io
    * this will probably need to be disabled in a couple of weeks because
    the amount of events could get very high but it will be useful for
    identifying issues early on

--- gambit/settings/base.py
+++ gambit/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
