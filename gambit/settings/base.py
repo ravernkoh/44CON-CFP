b29bbd99345477fa2701ea6553008a027325c01c
nimxj <github@aidamitchell.uk>
2018-02-06 08:07:25 +0000

    never assume you already did something; fixes #1

--- gambit/settings/base.py
+++ gambit/settings/base.py
@@ -188,6 +188,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home"
 
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
